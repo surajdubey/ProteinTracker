c046b2b28496e07d2fd978115608a4de7f1435ed
Suraj Dubey <suraj.dubey@hotmail.com>
2016-02-01 17:51:34 +0530

    Initial Commit

---
+++ ProteinTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProteinTracker
+//
+//  Created by ServesyOfficial on 01/02/16.
+//  Copyright © 2016 servesy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ProteinTracker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ProteinTracker
+//
+//  Created by ServesyOfficial on 01/02/16.
+//  Copyright © 2016 servesy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
